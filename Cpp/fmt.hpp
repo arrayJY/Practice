e7663fba9c1190d9159b8c56da29e161ebac6e2d
arrayJY <ji957455952@163.com>
2020-09-05 17:30:17 +0800

    ✨ Add values fmt.

--- Cpp/fmt.hpp
+++ Cpp/fmt.hpp
@@ -8,6 +8,7 @@
 #include <string_view>
 
 template <typename T> concept Str = requires(T s) { std::string_view(s); };
+template <typename T> concept ToStr = requires(T s) { std::to_string(s); };
 
 template <Str... S> std::string fmt(std::string_view format, S... strs) {
   std::string result;
@@ -20,6 +21,21 @@ template <Str... S> std::string fmt(std::string_view format, S... strs) {
   };
 
   (extend(strs), ...);
+
+  return result;
+}
+
+template <ToStr... S> std::string fmt(std::string_view format, S... strs) {
+  std::string result;
+
+  auto extend = [&format, &result](auto &&str) {
+    auto pos = format.find("{}");
+    result += format.substr(0, pos);
+    result += str;
+    format = format.substr(pos + 2);
+  };
+
+  (extend(std::to_string(strs)), ...);
 
   return result;
 }
